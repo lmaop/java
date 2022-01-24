8abdb3288af431d2fd413b278c97c804b02e28c2
Jason Jonathan David Pathripala <jasonjonathandavid@gmail.com>
2022-01-24 14:36:43 +0530

    java

---
+++ One.java
@@ -0,0 +1,10 @@
+class One{
+    public static void main(String args[]){
+        int len=args.length;
+        for(int i=0;i<len;i++)
+        {
+            System.out.println(i +"Argument"+args[i]);
+        }
+    }
+    
+}
